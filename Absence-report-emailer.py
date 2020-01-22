50e7f42456a10b9a66a5248f15f589e7d04c372e
TheCambridgeRed <dan.elbro@gmail.com>
2020-01-22 14:54:56 +0000

    Print to stdout
    A couple of changes and you can see it working now. I made a lot of mistakes - would be nice to read it off a spreadsheet. Until then, would be good to somehow keep the name of the last tutor given somewhere, so you can repeat it by pressing Enter, say, if two consecutive students have the same tutor.

--- Absence-report-emailer.py
+++ Absence-report-emailer.py
@@ -2,13 +2,13 @@
 Simple utility to draft emails to tutors of absent students.
 Currently set to not send, but only draft these.
 """
-import pyOutlook
+# import pyOutlook
 import datetime
 
 # Collect Student Names
 student_names = []
 while True:
-    newinput = input("Add student to reports - or type 'DONE' to continue to tutors, DELETE to remove last addition")
+    newinput = input("Add student to reports - or type 'DONE' to continue to tutors, DELETE to remove last addition\n")
     if newinput == "DONE":
         break
     if newinput == "DELETE":
@@ -22,25 +22,24 @@ while True:
 tutor_names = []
 tutor_addresses = []
 for student in student_names:
-    tutor_name = input("Name of Tutor for {student}")
+    tutor_name = input(f"Name of Tutor for {student}: ")
     tutor_names.append(tutor_name)
-    default_address = (".".join(tutor_name.split())) + "@kcl.ac.uk"
-    tutor_address = input("Email address for {tutor_name} (RETURN for {default_address})")
+    default_address = (".".join(tutor_name.split()).lower()) + "@kcl.ac.uk"
+    tutor_address = input(f"Email address for {tutor_name} (RETURN for {default_address})")
     if tutor_address == "":
         tutor_addresses.append(default_address)
     else:
         tutor_addresses.append(tutor_address)
 
 # Collect Seminar Name
-seminar_name = input("Enter Seminar Name (e.g. 'Methodology')")
+seminar_name = input("Enter Seminar Name (e.g. 'Methodology'): ")
 
 # Collect Seminar Time
-seminar_time = input("Enter Seminar Time (e.g. 'Wednesay, 11:00-12:00')")
+seminar_time = input("Enter Seminar Time (e.g. 'Wednesay, 11:00-12:00'): ")
 
 # Draft Outlook Emails to Tutors
 def create_email_body(student_name, tutor_name, seminar_name, seminar_time):
-    template =
-    f"""
+    template = f"""
     Dear {tutor_name.split()[0]},
 
     I am reporting the absence of <b>{student_name}</b> in two consecutive seminars. I just want to check they are alright.
@@ -51,13 +50,20 @@ def create_email_body(student_name, tutor_name, seminar_name, seminar_time):
     Gregor
     """
 
+    return template
+
+for i, student in enumerate(student_names):
+    email = create_email_body(student, tutor_names[i], seminar_name, seminar_time)
+    print(email)
+    print('\n---------------------------------------------\n')
+    
 # Save report of reported absences
 date = str(datetime.datetime.today())
-report =
-f"""
+newline = '\n'
+report = f"""
 Reported Absences
 
 on {date}
 
-{"\nl".join(student_names)}
+{newline.join(student_names)}
 """
